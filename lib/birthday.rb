36b2a443a7973d3ff4c36baceca6808a23b0824a
Codey Pinero <codey.pinero@gmail.com>
2019-03-26 16:26:14 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,6 +1,6 @@
 # birthday_kids = {
-#   "Timmy" => 9, 
-#   "Sarah" => 6, 
+#   "Timmy" => 9,
+#   "Sarah" => 6,
 #   "Amanda" => 27
 # }
 
@@ -8,4 +8,8 @@ def happy_birthday(birthday_kids)
   # add your code snippet here!
 end
 
+hash = {key1: "value1", key2: "value2"}
 
+hash.each do |key, value|
+  puts "#{key}: #{value}"
+end
